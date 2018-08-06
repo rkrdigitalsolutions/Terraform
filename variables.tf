edbac3532e329e815740e013a6ef11f053191818
root <root@kotireddy4782.mylabserver.com>
2018-08-06 22:09:11 +0000

    breaking out our variables definitions

--- variables.tf
+++ variables.tf
@@ -4,35 +4,19 @@ variable "env" {
 variable "image" {
   description = "image for container"
   type = "map"
-  default = {
-     dev = "ghost:latest"
-     prod = "ghost:latest"
-   }
 }
 
 variable "container_name" {
   description = "Name of Container"
   type = "map"
-  default = {
-     dev = "dev_blog"
-     prod = "prod_blog"
-   }
 }
 
 variable "int_port" {
   description = "Internal port for container"
   type = "map"
-  default = {
-     dev = "2368"
-     prod = "2368"
-   }
 }
 
 variable "ext_port" {
   description = "External port for container"
   type = "map"
-  default = {
-     dev = "8080"
-     prod = "80"
-   }
 }
