4f0b0c3465cedeb8b91334a88245b98a7cfdde2a
root <root@kotireddy4782.mylabserver.com>
2018-08-06 22:23:28 +0000

    Null Resources and Local-Exec

--- main.tf
+++ main.tf
@@ -13,3 +13,8 @@ module "container" {
   ext_port = "${lookup(var.ext_port, var.env)}"
 }
 
+resource "null_resource" "null_id" {
+  provisioner "local-exec" {
+    command = "echo ${module.container.container_name}:${module.container.ip} >> container.txt"
+   }
+}
